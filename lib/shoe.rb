18cd2fe0dc9599d4f45335e36a40d99e111902a1
Ryna Truong <rynatruong@outlook.com>
2020-03-04 00:15:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,13 +7,14 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS << brand
-    
-    
-    if BRANDS.include(brand)
-      @brand = brand
-    else BRANDS << brand
-    end
   end
+    
+    
+  #   if BRANDS.include(brand)
+  #     @brand = brand
+  #   else BRANDS << brand
+  #   end
+  # end
 
   def cobble
     self.condition = "new"
